2e49554b4c5e5c8a8ebc5326bd78f42aa5ed037a
Peter Jablonski <pjablons@conncoll.edu>
2014-06-24 23:00:58 -0400

    Inheritance works.

--- lib/SimNet/Frames.pm
+++ lib/SimNet/Frames.pm
@@ -2,24 +2,23 @@ use v6;
 
 module SimNet::Frames;
 
-#class SimNet::Frame {...};
-
 sub recursiveGet($slot, @queue, %visited) {
 	if @queue.elems > 0 {
 		my $frame = @queue.shift;
-		if !%visited{$frame}:exists {
+#		say $frame;
+		if !(%visited{$frame}:exists) {
 			%visited{$frame} = True;
-			my @ret = $frame.get($slot);
-			if @ret[0] ~~ "enqueue" {
+			my @ret = $frame.get($slot, False);
+			if @ret[0] ~~ :!defined {
+				recursiveGet($slot, @queue, %visited);
+			} elsif @ret[0] ~~ "enqueue" {
 				@queue.push(@ret[1]);
-				recursiveGet($slot, @queue, %visited, False);
-			} elsif @ret[0] ~~ Nil {
-				recursiveGet($slot, @queue, %visited, False);
+				recursiveGet($slot, @queue, %visited);
 			} else {
 				@ret[1];
 			}
 		} else {
-			recursiveGet($slot, @queue, %visited, False);
+			recursiveGet($slot, @queue, %visited);
 		}
 	} else {
 		False;
@@ -28,45 +27,50 @@ sub recursiveGet($slot, @queue, %visited) {
 
 class SimNet::Frame {
 	has	%.data;
+	has	$.id is rw;
 	method get($slot, $first=True) {
 		if $first {
 			my %visited{SimNet::Frame};
-			recursiveGet($slot, (self), %visited);
+			recursiveGet($slot, [self], %visited);
 		} elsif %.data{$slot}:exists {
-			["result", %.data{$slot}];
-		} elsif !%.data{"isa"}:exists {
-			[Nil];
+			("result", %.data{$slot});
+		} elsif !(%.data{"isa"}:exists) {
+			Nil;
 		} else {
-			["enqueue", %.data{"isa"}];
+#			say %.data{"isa"}[0];
+			("enqueue", %.data{"isa"}[0]);
 		}	
 	}
 
 	method put($key, $val, $overwrite=False) {
-		if !%.data{$key}:exists || $overwrite {
-			%.data{$key} = ();
+		if !(%.data{$key}:exists) || $overwrite {
+			%.data{$key} = @(());
 		}
 		push %.data{$key}, $val;
 	}
 	
 	method fromString($frame, $overwrite=True) {
-		say $frame;
 		my @framereqs = ();
 		my regex id { \d+ };
 		my regex frameref { '|<'\d+'>|' };
-		my regex valid { <[a..z]+[0..9]>+ };
+		my regex valid { :i <[a..z]+[0..9]>+ };
 		my regex pair { <&valid>'=>'(<&frameref>|<&valid>) };
-		say &id.perl;
-		my $id = +(($frame ~~ m/\d+/).Str);
+		my $id = +(($frame ~~ m/<&id>/).Str);
+		$.id = $id;
 		my @pairs = (m:g/<&pair>/ given $frame)>>.Str;
 		for @pairs -> $pair {
 			my ($key, $value) = $pair.split('=>');
 			if $value ~~ m/<&frameref>/ {
-				push @framereqs, $pair;
+				push @framereqs, $key => $value;
 			} else {
 				self.put($key, $value, $overwrite);
 			}
 		}
 		($id, @framereqs);
+	}
+
+	method WHICH() {
+		"SimNet::Frame|$.id";
 	}
 }
 
@@ -74,15 +78,21 @@ sub loadFrames($str) is export {
 	my @list = $str.split(' ');
 	my %framedex;
 	my @unsatisfied;
-#	say "Got this far...";
 	for @list {
 		my $frame = SimNet::Frame.new;
 		my ($id, @reqs) = $frame.fromString($_);
 		%framedex{$id} = $frame;
-		push @unsatisfied, [$id, @reqs];
+		if @reqs[0]:exists {
+			push @unsatisfied, [$id, [@reqs]];
+		}
 	}
-	for @unsatisfied {
-		%framedex{$_[0]}.put($_[1].key, %framedex{($_[1].value ~~ m/\d+/).Str});
+	for @unsatisfied -> @sub {
+		my $id = @sub[0];
+		for @sub[1] -> @pairs {
+			for @pairs {
+				%framedex{$id}.put($_.key, %framedex{($_.value ~~ m/\d+/).Str}); 
+			}
+		}
 	}
 	%framedex;
 }
--- t/Frames.t
+++ t/Frames.t
@@ -1,8 +1,9 @@
 use v6;
 use SimNet::Frames;
 
-my %frames = loadFrames(':0{testKey=>testVal,key2=>val2}: :1{isa=>|<1>|,name=>one}:');
-say %frames{0};
-
+my %frames = loadFrames(':0{testKey=>testVal,key2=>val2}: :1{isa=>|<0>|,name=>test}:');
+say %frames{0}.get("testKey");
+say %frames{1}.get("testKey");
+say %frames{1}.get("jkladg");
 
 # vim: ft=perl6
